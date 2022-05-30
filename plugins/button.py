5fe41f010de4556926b6dde57a249cb9120b3e6f
Rizzu Senpai <104607928+RizzuSen@users.noreply.github.com>
2022-05-30 13:32:17 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,13 +1,11 @@
-# Credits: @mrismanaziz
-# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
-# t.me/SharingUserbot & t.me/Lunatic0de
 
-from config import FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
+
+from config import FORCE_SUB_CHANNEL¹, FORCE_SUB_CHANNEL²
 from pyrogram.types import InlineKeyboardButton
 
 
 def start_button(client):
-    if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL¹ and not FORCE_SUB_CHANNEL²:
         buttons = [
             [
                 InlineKeyboardButton(text="• ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ •", callback_data="about"),
